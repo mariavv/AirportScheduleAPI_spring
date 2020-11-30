affcb676c3a8d878103a9c079241947b8f6e4415
maria <mv.vasilyeva@mediasoft.team>
2020-11-30 15:15:55 +0400

    Lombok, вроде пашет

--- src/main/java/mariavv/airportscheduleapispring/domain/entity/UserEntity.java
+++ src/main/java/mariavv/airportscheduleapispring/domain/entity/UserEntity.java
@@ -1,12 +1,16 @@
 package mariavv.airportscheduleapispring.domain.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
+@Setter
+@Getter
 public class UserEntity {
 
     @Id
@@ -15,46 +19,18 @@ public class UserEntity {
     private Integer id;
 
     @Column(name = "name")
-    @Size(min=2, max=50)
+    @Size(min = 2, max = 50)
     private String name;
 
     @Column(name = "password")
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable (name="user_roles",
-            joinColumns=@JoinColumn (name="user_id"),
-            inverseJoinColumns=@JoinColumn(name="role_id"))
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<RoleEntity> roles;
 
     public UserEntity() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<RoleEntity> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<RoleEntity> roles) {
-        this.roles = roles;
     }
 }
