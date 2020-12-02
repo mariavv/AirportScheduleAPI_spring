92318c510ea4900dfcfbfeaeb5458c8574948a29
maria <mv.vasilyeva@mediasoft.team>
2020-12-02 15:00:07 +0400

    @Mappings

--- src/main/java/mariavv/airportscheduleapispring/mapper/AnotherFlightMapper.java
+++ src/main/java/mariavv/airportscheduleapispring/mapper/AnotherFlightMapper.java
@@ -6,6 +6,7 @@ import mariavv.airportscheduleapispring.domain.entity.AirportEntity;
 import mariavv.airportscheduleapispring.domain.entity.FlightEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,23 +14,28 @@ import java.util.List;
 @Mapper(componentModel = "spring", imports = {AirportEntity.class})
 @Component
 public interface AnotherFlightMapper {
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "airportFrom", expression = "java(airportFrom)")
-    @Mapping(target = "airportTo", expression = "java(airportTo)")
-    @Mapping(target = "departure", expression = "java(flightDto.getDeparture())")
-    @Mapping(target = "arrival", expression = "java(flightDto.getArrival())")
-    @Mapping(target = "delay", expression = "java(flightDto.getDelay())")
-    @Mapping(target = "delayArrival", expression = "java(flightDto.getDelayArrival())")
-    @Mapping(target = "postponedOn", expression = "java(postponedOn)")
+    
+    @Mappings({
+    @Mapping(target = "id", ignore = true),
+    @Mapping(target = "airportFrom", expression = "java(airportFrom)"),
+    @Mapping(target = "airportTo", expression = "java(airportTo)"),
+    @Mapping(target = "departure", expression = "java(flightDto.getDeparture())"),
+    @Mapping(target = "arrival", expression = "java(flightDto.getArrival())"),
+    @Mapping(target = "delay", expression = "java(flightDto.getDelay())"),
+    @Mapping(target = "delayArrival", expression = "java(flightDto.getDelayArrival())"),
+    @Mapping(target = "postponedOn", expression = "java(postponedOn)"),
     @Mapping(target = "isCanceled", expression = "java(flightDto.getIsCanceled())")
+    })
     FlightEntity toFlightEntity(FlightDto flightDto, AirportEntity airportFrom, AirportEntity airportTo, FlightEntity postponedOn);
 
     @Mapping(target = "flight", source = "flightEntity")
     FlightWithIdDto toFlightWithIdDto(FlightEntity flightEntity);
 
-    @Mapping(target = "airportFromId", expression = "java(flightEntity.getAirportFrom().getId())")
-    @Mapping(target = "airportToId", expression = "java(flightEntity.getAirportTo().getId())")
-    @Mapping(target = "postponedOn", expression = "java(flightEntity.getPostponedOn() == null ? null : flightEntity.getPostponedOn().getId())")
+    @Mappings({
+            @Mapping(target = "airportFromId", expression = "java(flightEntity.getAirportFrom().getId())"),
+            @Mapping(target = "airportToId", expression = "java(flightEntity.getAirportTo().getId())"),
+            @Mapping(target = "postponedOn", expression = "java(flightEntity.getPostponedOn() == null ? null : flightEntity.getPostponedOn().getId())")
+    })
     FlightDto toFlightDto(FlightEntity flightEntity);
 
     List<FlightWithIdDto> toDtoList(List<FlightEntity> entityList);
